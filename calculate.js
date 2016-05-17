065e0fba7a0e9c17ba67b120ce8beb9c2a278e99
Janet Mardjuki <mardjuki@sfu.ca>
2016-05-17 12:05:55 -0700

    Added calculation for both buttons

--- calculate.js
+++ calculate.js
@@ -1,6 +1,8 @@
 "use strict"
 
-function avgCalc() {
+function meanCalc() {
+  var sum = 0;    // Save the sum
+  var count = 0;
   for (var i = 1; i <= 4; i++) {
     var nameStr = "act" + i.toString();
     var nameObj = document.getElementsByName(nameStr);
@@ -8,9 +10,28 @@ function avgCalc() {
       // If one of them are not defined, skip it
       var num = nameObj[0].value;
       var den = nameObj[1].value;
-      console.log(num/den); // Write on the percantage column
+      sum = sum + (num/den);
+      count = count + 1;
     }
   }
+  console.log(sum/count);
+}
+
+function avgCalc() {
+  var sumN = 0;    // Save the sum
+  var sumD = 0;
+  for (var i = 1; i <= 4; i++) {
+    var nameStr = "act" + i.toString();
+    var nameObj = document.getElementsByName(nameStr);
+    if (nameObj[0].value | nameObj[1].value) {
+      // If one of them are not defined, skip it
+      var num = nameObj[0].value;
+      var den = nameObj[1].value;
+      sumN = sumN + num;
+      sumD = sumD + den;
+    }
+  }
+  console.log(sumN/sumD);
 }
 
 function percentageCalc() {
