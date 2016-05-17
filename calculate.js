abc0dbfa50d6723333d291c6ed170f498deafdfb
Janet Mardjuki <mardjuki@sfu.ca>
2016-05-17 12:40:41 -0700

    Added blur for act1_boxes

--- calculate.js
+++ calculate.js
@@ -1,4 +1,27 @@
-"use strict"
+(function() {
+  // document ready
+
+  // Improve the following using for loop
+  var act1_box1 = document.getElementById("act1_box1");
+  var act1_box2 = document.getElementById("act1_box2");
+  act1_box1.addEventListener("blur", function (){
+    var nameObj = document.getElementsByName("act1");
+    if (nameObj[0].value && nameObj[1].value) {
+      var num = nameObj[0].value;
+      var den = nameObj[1].value;
+      console.log(num/den); // Change Percentage, and trim (2 sig figures)
+    }
+  }, true);
+  act1_box2.addEventListener("blur", function (){
+    var nameObj = document.getElementsByName("act1");
+    if (nameObj[0].value && nameObj[1].value) {
+      var num = nameObj[0].value;
+      var den = nameObj[1].value;
+      console.log(num/den); // Change Percentage, and trim (2 sig figures)
+    }
+  }, true);
+
+})();
 
 function meanCalc() {
   var sum = 0;    // Save the sum
@@ -6,11 +29,12 @@ function meanCalc() {
   for (var i = 1; i <= 4; i++) {
     var nameStr = "act" + i.toString();
     var nameObj = document.getElementsByName(nameStr);
-    if (nameObj[0].value | nameObj[1].value) {
+    if (nameObj[0].value || nameObj[1].value) {
       // If one of them are not defined, skip it
       var num = nameObj[0].value;
       var den = nameObj[1].value;
       sum = sum + (num/den);
+      // Fill the percentage too, user might change
       count = count + 1;
     }
   }
