4837e77bd2b031f9f1d074304f52b4fbfd9f481e
Janet Mardjuki <mardjuki@sfu.ca>
2016-05-17 23:54:26 -0700

    Trimmed layout in css Added JavaScript core implementation for blur, average, and mean Added IDs on HTML

--- calculate.js
+++ calculate.js
@@ -1,64 +1,92 @@
+"use strict";
+
 (function() {
-  // document ready
-
-  // Improve the following using for loop
-  var act1_box1 = document.getElementById("act1_box1");
-  var act1_box2 = document.getElementById("act1_box2");
-  act1_box1.addEventListener("blur", function (){
-    var nameObj = document.getElementsByName("act1");
-    if (nameObj[0].value && nameObj[1].value) {
-      var num = nameObj[0].value;
-      var den = nameObj[1].value;
-      console.log(num/den); // Change Percentage, and trim (2 sig figures)
+  // Document Ready
+  // FEATURE: Calculate the result as user fill activity boxes
+  for (var i = 1; i <= 4; i++) {
+    (function() {
+    var nameStr = "act" + i.toString();
+    var percentageId = 'percentage-' + i.toString();
+    for (var j = 1; j <= 2; j++) {
+      var idStr = nameStr + "_box" + j.toString();
+      var actBox = document.getElementById(idStr);
+      actBox.addEventListener("blur", function (){
+        var nameObj = document.getElementsByName(nameStr);
+        if (nameObj[0].value && nameObj[1].value) {
+          var num = nameObj[0].value;
+          var den = nameObj[1].value;
+          var result = num/den * 100;
+          var percentageBox = document.getElementById(percentageId);
+          percentageBox.innerHTML = '<span>' + result.toString() + '</span>';
+        }
+      }, true);
     }
-  }, true);
-  act1_box2.addEventListener("blur", function (){
-    var nameObj = document.getElementsByName("act1");
-    if (nameObj[0].value && nameObj[1].value) {
-      var num = nameObj[0].value;
-      var den = nameObj[1].value;
-      console.log(num/den); // Change Percentage, and trim (2 sig figures)
-    }
-  }, true);
-
+    })();
+  }
 })();
 
 function meanCalc() {
   var sum = 0;    // Save the sum
   var count = 0;
+  var alertString;
+  var result;
   for (var i = 1; i <= 4; i++) {
     var nameStr = "act" + i.toString();
     var nameObj = document.getElementsByName(nameStr);
-    if (nameObj[0].value || nameObj[1].value) {
-      // If one of them are not defined, skip it
+    if (nameObj[0].value && nameObj[1].value) {
       var num = nameObj[0].value;
       var den = nameObj[1].value;
       sum = sum + (num/den);
       // Fill the percentage too, user might change
       count = count + 1;
     }
+    else if ( nameObj[0].value || nameObj[1].value ) {
+      alertString = "WARNING: Activity " + i.toString() + " is incomplete, therfore, it will not be used for mean calculation."
+      alert(alertString)
+    }
   }
-  console.log(sum/count);
+  // Write result, if denum is zero alert user
+  // iF count less than equal to zero don't write
+  if ( count !== 0 ) {
+    result = sum/count * 100;
+    var totalBox = document.getElementById("percentage-total");
+    totalBox.innerHTML = '<span>' + result.toString() + '</span>';
+  }
+  else {
+    alert("ERROR: No activity for mean calculation");
+  }
 }
 
 function avgCalc() {
   var sumN = 0;    // Save the sum
   var sumD = 0;
+  var alertString;
+  var count = 0;
   for (var i = 1; i <= 4; i++) {
     var nameStr = "act" + i.toString();
     var nameObj = document.getElementsByName(nameStr);
-    if (nameObj[0].value | nameObj[1].value) {
+    if (nameObj[0].value && nameObj[1].value) {
       // If one of them are not defined, skip it
       var num = nameObj[0].value;
       var den = nameObj[1].value;
       sumN = sumN + num;
       sumD = sumD + den;
+      count = count + 1;
+    }
+    else if ( nameObj[0].value || nameObj[1].value ) {
+      alertString = "WARNING: Activity " + i.toString() + "is incomplete, therfore, it will not be used for the average"
+      alert(alertString)
     }
   }
-  console.log(sumN/sumD);
-}
+  // Write result, if denum is zero alert the user
+  // iF count less than equal to zero don't write
+  if ( count !== 0 ) {
+    result = sumN/sumD * 100;
+    var totalBox = document.getElementById("percentage-total");
+    totalBox.innerHTML = '<span>' + result.toString() + '</span>';
+  }
+  else {
+    alert("ERROR: No activity for average calculation");
+  }
 
-function percentageCalc() {
-  var num = document.getElementsByName("act1")[0].value;
-  var den = document.getElementsByName("act1")[1].value;
 }
